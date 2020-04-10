8f27ff586bd69239abfcc7615ab8da7963dd9b7d
Andrew Haberman <a.haberman25@gmail.com>
2020-04-10 19:38:08 -0400

    set up fetch and basic information

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      githubUsers: []
+    };
+  }
+
+  componentDidMount() {
+    console.log('mount')
+
+    fetch(`https://api.github.com/users/ahaberman25`)
+      .then(res => res.json())
+      .then(users => {        
+        console.log('users: ', users)
+        this.setState({ githubUsers: users });
+      })
+      .catch(err => {
+        console.log('err: ', err)
+      })
+  }
+
+  render() {
+    console.log('state: ', this.state.githubUsers)
+    return (
+      <div>
+        <p>{this.state.githubUsers.login}</p>
+        <p>{this.state.githubUsers.id}</p>
+        <img src={this.state.githubUsers.avatar_url} alt={this.state.githubUsers.avatar_url} key={this.state.githubUsers.avatar_url} />
+        <p>{this.state.githubUsers.html_url}</p>
+        <p>{this.state.githubUsers.name}</p>
+        <p>{this.state.githubUsers.company}</p>
+        <p>{this.state.githubUsers.location}</p>
+        <p>{this.state.githubUsers.public_repos}</p>
+        <p>{this.state.githubUsers.followers}</p>
+        <p>{this.state.githubUsers.following}</p>
+        <p>{this.state.githubUsers.created_at}</p>
+      </div>
+    )
+  }
 }
 
 export default App;
